e4734ebaf0a3472fd0d166afe7dd47befd70993c
duyahui <duyahui@xiaomi.com>
2020-06-16 19:55:08 +0800

    init code

---
+++ init.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (auto-complete counsel ace-window tabbar org-bullets which-key try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+        		 ("org-cn"   . "http://elpa.emacs-china.org/org/")
+        		 ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
